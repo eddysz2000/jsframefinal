6bd167912db3f35fba582d019d766b5d7d4a9aa6
GRUPOAMSA\esanchez <eddysz2000@gmail.com>
2019-01-02 12:34:35 -0500

    agregar funcion random
    agregar imagenes en random

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 $(function(){
-colorTituloBlanco($('.main-titulo'))
+	//cambiar color de titulo principal
+	colorTituloBlanco($('.main-titulo'));
+
+	//llenar elementos en tablero
+
+	agregarElemento($('.col-1'));
+
+	
+
 })
 
 
@@ -19,4 +27,48 @@ color: '#DCFF0E'
 }, 10, function(){
 colorTituloBlanco(elemento);
 })
+}
+
+//Funcion que agrega en forma aleatoria los elementos en el tablero
+function agregarElemento(elemento){
+	var i;
+	var r;
+	for (i=0;i<7;i++){
+		r=Math.floor((Math.random() * 10) + 1);
+
+		switch (r){
+			case 1:
+				$(elemento).append('<img src="image/1.png" class="elemento"/>');
+			break;
+			case 2:
+				a$(elemento).append('<img src="image/1.png" class="elemento"/>');
+			break;
+			case 3:
+				$(elemento).append('<img src="image/1.png" class="elemento"/>');
+			break;
+			case 4:
+				$(elemento).append('<img src="image/1.png" class="elemento"/>');
+			break;
+			case 5:
+				$(elemento).append('<img src="image/1.png" class="elemento"/>');
+			break;
+			case 6:
+				$(elemento).append('<img src="image/1.png" class="elemento"/>');
+			break;
+			case 7:
+				$(elemento).append('<img src="image/1.png" class="elemento"/>');
+			break;
+			case 8:
+				$(elemento).append('<img src="image/1.png" class="elemento"/>');
+			break;
+			case 9:
+				$(elemento).append('<img src="image/1.png" class="elemento"/>');
+			break;
+			case 10:
+				$(elemento).append('<img src="image/1.png" class="elemento"/>');
+			break;
+		}
+
+	}
+
 }
