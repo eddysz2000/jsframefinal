c5512f34bbcfd12f13f1e5f891f768908dab2371
GRUPOAMSA\esanchez <eddysz2000@gmail.com>
2019-01-07 12:27:55 -0500

    validacion de imagenes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(function(){
 
 	var clicks=0;
+	var movimientos=0;
 
 	//cambiar color de titulo principal
 	colorTituloBlanco($('.main-titulo'));
@@ -96,4 +97,132 @@ function numeroAleatorio(min, max) {
 function imagenAletoria(){
   var sources = ['image/1.png', 'image/2.png', 'image/3.png', 'image/4.png'];
   return sources[numeroAleatorio(0,3)]
+}
+
+//Función auxiliar que suma los movimientos del jugador
+function sumarMovimiento(){
+  movimientos++;
+  $('#movimientos-text').text(movimientos);
+}
+
+//Función auxiliar que compara si dos elementos Dulce son iguales (checkSrc)
+function validaImagen(elemento1, elemento2){
+  if ($(elemento1).attr("src")==$(elemento2).attr("src")) {
+    return true;
+  }else return false;
+}
+
+//Función que adiciona los puntos y esconde los elementos en línea (punto)
+function puntos(elemento1, elemento2, elemento3){
+  puntuacion= puntuacion + 10;
+  $("#score-text").text(puntuacion);
+  $(elemento1).hide('pulsate', 2000)
+  $(elemento2).hide('pulsate', 2000)
+  $(elemento3).hide('pulsate', 2000)
+}
+
+//Función auxiliar que elimina los elementos escondidos de la estructura del DOM
+function eliminarElementos(){
+  $("img:hidden").each(function(index){
+      $(this).remove()
+  })
+}
+
+//Función que llena cada columna con los elementos faltantes
+function llenarDespuesTurno(){
+  var numeroElementos = numeroFalta = 0;
+  for (var i = 1; i <= 7; i++) {
+    numeroElementos=$(".col-"+i).find("img").length;
+    numeroFalta = 7 - numeroElementos;
+    fillElemento($(".col-"+i), numeroFalta);
+  }
+  window.setTimeout(postJugada, 500)
+}
+
+//Función que recorre todo el tablero verificando si hay match de 3 dulces o mas, horizontal y verticalmente
+function checkMatch(){
+  var elementoCompara;
+  var actual;
+  var matchIzquierda = false;
+  var matchDerecha = false;
+  var matchAbajo = false;
+  var matchArriba = false;
+  for (var col = 1; col <= 7; col++) {
+    for (var row = 0; row < 7; row++) {
+      matchArriba=matchAbajo=matchDerecha=matchIzquierda=false;
+      actual = $(".col-"+col).find("img")[row]
+
+      //Verficacion a la Izquierda
+      if($(".col-"+(col-1)).length > 0){ //Verifica si existe elemento a la izquierda
+        elementoCompara = $(".col-"+(col-1)).find("img")[row]
+        if (checkSrc(actual, elementoCompara)) {
+          matchIzquierda = true;
+          if($(".col-"+(col-2)).length > 0){ //Verifica si existen dos columnas a la izquierda
+            elementoCompara = $(".col-"+(col-2)).find("img")[row]
+            if(checkSrc(actual, elementoCompara)){
+              punto(actual, $(".col-"+(col-1)).find("img")[row], elementoCompara )
+
+            }
+          }
+        }
+      }
+
+      //Verificacion a la Derecha
+      if($(".col-"+(col+1)).length > 0){ //Verifica si existe elemento a la izquierda
+        elementoCompara = $(".col-"+(col+1)).find("img")[row]
+        if (checkSrc(actual, elementoCompara)) {
+          matchDerecha = true;
+          if($(".col-"+(col+2)).length > 0){ //Verifica si existen dos columnas a la izquierda
+            elementoCompara = $(".col-"+(col+2)).find("img")[row]
+            if(checkSrc(actual, elementoCompara)){
+              punto(actual, $(".col-"+(col+1)).find("img")[row], elementoCompara )
+
+            }
+          }
+        }
+      }
+
+      //Verificacion ambos izquierda y Derecha
+      if (matchIzquierda == true && matchDerecha == true) {
+        punto(actual, $(".col-"+(col-1)).find("img")[row], $(".col-"+(col+1)).find("img")[row])
+
+      }
+      //Verificación hacia arriba
+      if($(".col-"+col).find("img")[row-1]){ //Verifica si existe elemento arriba
+        elementoCompara = $(".col-"+col).find("img")[row-1]
+        if (checkSrc(actual, elementoCompara)) {
+          matchArriba = true;
+          if($(".col-"+col).find("img")[row-2]){ //Verifica si existen dos filas hacia arriba
+            elementoCompara = $(".col-"+col).find("img")[row-2]
+            if(checkSrc(actual, elementoCompara)){
+              punto(actual, $(".col-"+col).find("img")[row-1], elementoCompara)
+
+            }
+          }
+        }
+      }
+
+      //Verificacion hacia abajo
+      if($(".col-"+col).find("img")[row+1]){ //Verifica si existe elemento abajo
+        elementoCompara = $(".col-"+col).find("img")[row+1]
+        if (checkSrc(actual, elementoCompara)) {
+          matchAbajo = true;
+          if($(".col-"+col).find("img")[row+2]){ //Verifica si existen dos filas hacia abajo
+            elementoCompara = $(".col-"+col).find("img")[row+2]
+            if(checkSrc(actual, elementoCompara)){
+              punto(actual, $(".col-"+col).find("img")[row+1], elementoCompara)
+
+            }
+          }
+        }
+      }
+      //Verificacion ambos Arriba y Abajo
+      if (matchArriba == true && matchAbajo == true) {
+        punto(actual, $(".col-"+col).find("img")[row+1], $(".col-"+col).find("img")[row-1])
+
+      }
+    }
+
+
+  }
 }
